299d19e4b7c7fbc7f45c479bad36a334e0be826a
OlViks <salova.40@yandex.ru>
2022-09-15 02:14:36 +0300

    дорабтки примечаний

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
         double dog = 8;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
-        dog=dog + 4 -3.5;
-        cat=cat+4- 1.6;
-        paper=paper+4- 7639;
+        System.out.println("Задание 1.2");
+        dog=dog + 4;
+        cat=cat + 4;
+        paper=paper+4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println("Задание 1.3");
+        var dogSecond = dog - 3.5;
+        var catSecond = cat - 1.6;
+        var papersSecond =paper - 7639;
+        System.out.println(dogSecond);
+        System.out.println(catSecond);
+        System.out.println(papersSecond);
+        System.out.println("Задание 2");
         var friend = 19;
+        System.out.println(friend);
         friend = friend + 2;
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+        System.out.println("Задание 2.1");
         var frog = 3.5;
         System.out.println(frog);
         frog = frog * 10;
@@ -26,6 +37,7 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+        System.out.println("Задание 3");
         var firstBoxer = 78.2;
         var secondBoxer = 82.7;
         var sumBoxer = firstBoxer + secondBoxer;
