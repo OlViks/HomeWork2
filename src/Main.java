303db7f2332c61f412afa2df513415798c2074aa
OlViks <salova.40@yandex.ru>
2022-09-15 00:58:03 +0300

    Добавил вычитание

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        var dog = 8;
+        double dog = 8;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
-        dog=dog + 4;
-        cat=cat+4;
-        paper=paper+4;
+        dog=dog + 4 -3.5;
+        cat=cat+4- 1.6;
+        paper=paper+4- 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
